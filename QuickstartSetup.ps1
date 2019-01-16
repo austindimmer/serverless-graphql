f08cc7574c1c864d8ee4b6528a8b5c8c108f646d
Unknown <adi@emakina.com>
2019-01-16 23:01:54 +0100

    Initial code

---
+++ QuickstartSetup.ps1
@@ -0,0 +1,12 @@
+﻿cls
+python -m pip install --upgrade pip
+pip install awscli --upgrade --user
+
+aws --version
+python --version
+
+npm install -g serverless
+npm install -g yarn
+npm install -g netlify-cli
+
+yarn install
